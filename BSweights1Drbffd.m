3c093da5d8b4954835450c6a5a7c7253470f04ce
Slobodan Milovanovic <bluesboba@gmail.com>
2016-11-05 11:49:24 +0100

    Remove junk from BSweights1Drbffd

--- BSweights1Drbffd.m
+++ BSweights1Drbffd.m
@@ -75,12 +75,3 @@ lc(n+1,1)=-r;
 
 wc=Ac\lc;
 end
-
-function arg = getParforArg()
-p = gcp('nocreate'); % get the pool object if it exists, but never open a pool
-if isempty(p)
-  arg = 0;
-else
-  arg = p.NumWorkers;
-end
-end
