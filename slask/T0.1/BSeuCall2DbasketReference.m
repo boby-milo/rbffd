96dd1cd6b19d01a998072d8ddb20bc7cc2adccf1
Slobodan Milovanovic <bluesboba@gmail.com>
2017-03-09 19:48:57 +0100

    Update runs.

--- slask/T0.1/BSeuCall2DbasketReference.m
+++ slask/T0.1/BSeuCall2DbasketReference.m
@@ -9,11 +9,9 @@ Nx1 = round(0.9*Nx);
 
 M = 10^5;
 
-Nx = 40;
-Nx1 = round(0.9*Nx);
-M = 100;
-
+display(1)
 [u1,~,tim1,x1,dx1,n1,N1,W1] = BSeuCall2Dbasket_FD(Nx1,M,Kmul);
+display(2)
 [u,~,tim,x,dx,n,N,W] = BSeuCall2Dbasket_FD(Nx,M,Kmul);
 
 
